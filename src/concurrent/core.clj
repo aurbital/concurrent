ca04274b3b843833462e2a12635a02ac6b2ec22a
peilan <peilan.hsu@gmail.com>
2021-01-29 12:45:31 -0800

    added ant agent and integer random walk

--- src/concurrent/core.clj
+++ src/concurrent/core.clj
@@ -27,34 +27,59 @@
           (println (str "counter: " counter))
           (recur (dec counter))))))
 
+
+; create an agent that keeps track of its position in a map using :x and :y
+(defn create-ant
+  "returns an agent that is represented as a position, which in turn is represented as a map with :x and :y"
+  []
+  (agent {:x 0 :y 0}))
+
+(defn randomstep
+  "This is the update function to be sent to ant (agent).\n Input: position as represented by a map\n Output: position as represented by a map"
+  [{xpos :x ypos :y}]
+  (letfn [(rand1 []
+                 "random integer between -1 and +1, inclusive"
+                 (+ (rand-int 3) -1))]
+    {:x (+ xpos (rand1)) :y (+ ypos (rand1))}))
+
+; (randomstep {:x 0 :y 0})
+; (def ant (create-ant))
+; (let [ant (create-ant)]
+;   (send ant randomstep))
+
+(defn ant-walk
+  "Ant agent walk n steps"
+  [n]
+  (let [ant (create-ant)]
+    (loop [counter n]
+      (when (pos? counter)
+        (do
+          (send ant randomstep)
+          ; (println (str "counter: " counter))
+          (recur (dec counter)))))
+    (print ant)))
+
+
+
+        ; (send ant randomstep)
+
+; (ant-walk 100)
+
+
+
+
 (defn -main
   "Experiment to test code for concurrent processes"
   [& args]
-  ;--
-  ; desired result: see the interleaving of printing between threads
 
-  ; ******* use this one *******
-  ; Atom REPL: outputs nil
-  ; console REPL: prints here
-  ; runs with no errors
-  ; *** use this one: (.start (Thread. <some function that takes no arguments>))
-  ; i.e. (.start (Thread. pl100)) and not
-  ;      (.start (Thread. (pl100)))
-  (.start (Thread. pl100))
-  ; ***************************
+  ;(.start (Thread. pl100))
+  ;(.start (Thread. (fn [] (pl-n 100))))
 
-  ; ******* or this one *******
-  ; per feedback from stackoverflow
-  ; Thread.run requires a function of no inputs
-  ; so send it an anonymous function that takes care of all the input parameters you need
-  ; can't do nested #, so just use (fn ..)
-  ; this code outputs to console REPL and runs with no errors
-  (.start (Thread. (fn [] (pl-n 100))))
-  ; ***************************
 
-  ; ******* these will also work *******
-  ; note that Clojure functions already implement the Runnable interface
-  ; so #(.run <func>) is not necessary
-  (.start (Thread. #(.run (fn [] (pl-n 100)))))
-  (.start (Thread. #(.run pl100))))
-  ; ************************************
+  (.start (Thread. (fn [] (ant-walk 50))))
+  (.start (Thread. (fn [] (ant-walk 50))))
+  (.start (Thread. (fn [] (ant-walk 50))))
+  (.start (Thread. (fn [] (ant-walk 50))))
+  (.start (Thread. (fn [] (ant-walk 50)))))
+
+; (-main)
